0718ae01d8c21a3693b6cc4793590b2efd9c964f
Vatroslav Vrbanic <vatro@users.noreply.github.com>
2020-11-21 00:34:52 +0100

    Fixes Issue #1
    Removing SvelthreeInteraction.svelte from exports fixes issue #1

--- src/svelthree.d.ts
+++ src/svelthree.d.ts
@@ -11,16 +11,7 @@ export { default as HemisphereLight } from "./components/HemisphereLight.svelte"
 export { default as PointLight } from "./components/PointLight.svelte"
 export { default as Scene } from "./components/Scene.svelte"
 export { default as LoadedGLTF } from "./components/LoadedGLTF.svelte"
-
-/*
-The attempt to generate WebGLRenderer.d.ts from WebGLRenderer.svelte component's ts-code: 
-- this line links to WebGLRenderer.d.ts file, but is useless not wrapped into a 'SvelteComponent' class or similar
-  @see corresponding comment inside rollup.config.js
-*/
-// export * as WebGLRenderer from "./components/WebGLRenderer"
-
 export { default as WebGLRenderer } from "./components/WebGLRenderer.svelte"
-export { default as SvelthreeInteraction } from "./components/SvelthreeInteraction.svelte"
 export { default as OrbitControls } from "./components/OrbitControls.svelte"
 export { default as SessionAR } from "./components/SessionAR.svelte"
 export { default as SessionVR } from "./components/SessionVR.svelte"
