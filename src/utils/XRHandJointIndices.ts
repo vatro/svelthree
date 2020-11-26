663a695b1763f06475bc9e45fa51038503c11dac
Vatroslav Vrbanic <vatro@users.noreply.github.com>
2020-11-27 00:29:13 +0100

    Update XRHandJointIndices.ts

--- src/utils/XRHandJointIndices.ts
+++ src/utils/XRHandJointIndices.ts
@@ -4,4 +4,11 @@ export default class XRHandJointIndices {
     static INTERMEDIATE = [7, 12, 17, 22]
     static PROXIMAL = [2, 6, 11, 16, 21]
     static METACARPAL = [1, 5, 10, 15, 20]
+
+    static ALL = XRHandJointIndices.METACARPAL.concat(
+        XRHandJointIndices.PROXIMAL,
+        XRHandJointIndices.INTERMEDIATE,
+        XRHandJointIndices.DISTAL,
+        XRHandJointIndices.TIP
+    )
 }
