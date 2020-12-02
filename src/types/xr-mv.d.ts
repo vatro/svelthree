8f972183d4d169b4a4ed444d7f6a54bc5c59b452
Vatroslav Vrbanic <vatro@users.noreply.github.com>
2020-12-02 13:26:47 +0100

    Reorganize type definition files + prettier (120)

--- src/types/xr-mv.d.ts
+++ src/types/xr-mv.d.ts
@@ -4,171 +4,12 @@
  * Licensed under the Apache License, Version 2.0 (the 'License')
  */
 
-// ------------------ added by svelthree ---------------------
-
-declare type XRHitTestMode = "realworld" | "virtual"
-declare type SessionVRInputType = "controller" | "hand"
-
-declare type XRHandPinchMode = "remote" | "touch" | "hybrid"
-
-declare type LineDashedMaterial = import('svelthree-three').LineDashedMaterial
-declare type LineBasicMaterial = import('svelthree-three').LineBasicMaterial
-
-declare type XRHandPinchRayMaterial = LineDashedMaterial | LineBasicMaterial
-
-
-declare type Scene = import('svelthree-three').Scene
-declare type Raycaster = import('svelthree-three').Raycaster
-
-declare interface XRTouchRayUpdateParams {
-    handProfile: XRHandProfile
-    raycaster: Raycaster
-    xrFrameDelta: number
-}
-
-declare interface XRTouchSphereUpdateParams {
-    handProfile: XRHandProfile
-    raycaster: Raycaster
-    xrFrameDelta: number
-}
-
-//{hand: "left", mode:"hybrid",  distance: {remote: 2, touch: 0.01}, materials: {remote: pinchRemoteLineMat, touch: pinchTouchLineMat}, colors: {remote: 0xffffff, touch: 0xff0000}},
-declare interface XRHandPinchConfigItem {
-    hand: XRHandTouchEnabled
-    mode: XRHandPinchMode
-    distance?: {
-        remote?: number
-        touch?: number
-    }
-    materials?: {
-        remote?: XRHandPinchRayMaterial
-        touch?: XRHandPinchRayMaterial
-    }
-    colors?: {
-        remote?: number
-        remoteHit?: number
-        touch?: number
-        touchHit?: number
-    }
-}
-
-declare interface XRHandPinchConfig extends Array<XRHandPinchConfigItem> { }
-
-declare type XRHandProfile = "boxes" | "spheres" | "oculus"
-declare type XRHandTouchEnabled = "left" | "right" | "both"
-
-declare interface XRHandTouchConfigHandsItem {
-    hand: XRHandTouchEnabled
-    index: number[]
-}
-
-declare interface XRHandTouchConfigHands extends Array<XRHandTouchConfigHandsItem> { }
-
-declare type XRHandTouchTestMode = "ray" | "sphere"
-
-
-declare interface XRHandTouchRayDebuggerConfig {
-    drawTentacles: boolean
-    // TODO: Rename this
-    drawTouchDebuggers: boolean
-}
-
-declare interface XRHandTouchSphereDebuggerConfig {
-    widthSegments: number
-    heightSegments: number
-    colors: {
-        touch: number
-    }
-    mat: { [key: string]: any }
-}
-
-declare interface XRHandTouchDebugConfigItem {
-    mode: XRHandTouchTestMode,
-    config?: XRHandTouchRayDebuggerConfig | XRHandTouchSphereDebuggerConfig
-}
-
-declare interface XRHandTouchDebugConfig extends Array<XRHandTouchDebugConfigItem> { }
-
-declare interface XRHandTouchDebugParams {
-    enabled: boolean
-    debugConfig: XRHandTouchDebugConfig
-    hightlightJoints?: {
-        enabled: boolean
-        colors?: {
-            normal?: number
-            touch?: number
-        }
-    },
-    colorFaces?: {
-        enabled: boolean
-        colors?: {
-            touch?: number
-            unTouch?: number
-            touchThroughEnter?: number
-            touchThroughExit?: number
-            scratch?: number
-        }
-    }
-}
-
-declare interface XRHandTouchConfig {
-    mode?: XRHandTouchTestMode
-    sphereRadius?: number
-    touchDistance?: number
-    lerpFactor?: number
-    debug?: XRHandTouchDebugParams
-    hands?: XRHandTouchConfigHands
-}
-
-declare interface XRHandTouchXIndexItem {
-    left: number[], right: number[] | number[]
-}
-
-declare interface XRHandTouchXIndexPairs extends Array<XRHandTouchXIndexItem> { }
-
-declare interface XRHandTouchXConfigItem {
-    hand: XRHandTouchEnabled, name: string, distance?: number, touchtime?: number, indexPairs: XRHandTouchXIndexPairs
-}
-
-declare interface XRHandTouchXConfig extends Array<XRHandTouchXConfigItem> { }
-
-declare interface XRHandTouchEventsItem {
-    name: string, hand: XRHandTouchEnabled, distance?: number, touchtime?: number, index: number[]
-}
-
-declare interface XRHandTouchEvents extends Array<XRHandTouchEventsItem> { }
-
-declare interface XRSessionEvent extends Event {
-    readonly session: XRSession
-}
-
-//@see https://github.com/BabylonJS/Babylon.js/blob/master/src/LibDeclarations/webxr.d.ts
-declare type XREventType =
-    | "devicechange"
-    | "visibilitychange"
-    | "end"
-    | "inputsourceschange"
-    | "select"
-    | "selectstart"
-    | "selectend"
-    | "squeeze"
-    | "squeezestart"
-    | "squeezeend"
-    | "reset";
-
-//------------------------------------------------------------
-
 declare type Constructor<T = object> = {
-    new(...args: any[]): T
+    new (...args: any[]): T
     prototype: T
 }
 
-declare type XRReferenceSpaceType =
-    | "local"
-    | "local-floor"
-    | "bounded-floor"
-    | "unbounded"
-    | "viewer"
+declare type XRReferenceSpaceType = "local" | "local-floor" | "bounded-floor" | "unbounded" | "viewer"
 
 declare type XRSessionMode = "inline" | "immersive-ar" | "immersive-vr"
 
@@ -205,7 +46,7 @@ declare interface XRRigidTransform {
     readonly inverse: XRRigidTransform
 }
 
-declare interface XRSpace extends EventTarget { }
+declare interface XRSpace extends EventTarget {}
 
 declare interface XRReferenceSpace extends XRSpace {
     getOffsetReferenceSpace(originOffset: XRRigidTransform): XRReferenceSpace
@@ -302,9 +143,7 @@ declare interface XRSession extends EventTarget {
     renderState: XRRenderState
     updateRenderState(state?: XRRenderStateInit): any
     requestReferenceSpace(type: XRReferenceSpaceType): Promise<XRReferenceSpace>
-    requestHitTestSource(
-        options: XRHitTestOptionsInit
-    ): Promise<XRHitTestSource>
+    requestHitTestSource(options: XRHitTestOptionsInit): Promise<XRHitTestSource>
     requestHitTestSourceForTransientInput(
         options: XRTransientInputHitTestOptionsInit
     ): Promise<XRTransientInputHitTestSource>
@@ -321,7 +160,7 @@ declare interface XRViewport {
     readonly height: number
 }
 
-declare interface XRLayer { }
+declare interface XRLayer {}
 
 declare interface XRWebGLLayerInit {
     antialias?: boolean
@@ -337,11 +176,7 @@ declare class XRWebGLLayer implements XRLayer {
     public framebufferWidth: number
     public framebufferHeight: number
 
-    constructor(
-        session: XRSession,
-        gl: WebGLRenderingContext,
-        options: XRWebGLLayerInit
-    )
+    constructor(session: XRSession, gl: WebGLRenderingContext, options: XRWebGLLayerInit)
 
     getViewport(view: XRView): XRViewport
 }
---
+++ src/types/xr-svelthree.d.ts
@@ -0,0 +1,187 @@
+declare type Scene = import("svelthree-three").Scene
+declare type Raycaster = import("svelthree-three").Raycaster
+declare type LineDashedMaterial = import("svelthree-three").LineDashedMaterial
+declare type LineBasicMaterial = import("svelthree-three").LineBasicMaterial
+
+// based on @see https://github.com/BabylonJS/Babylon.js/blob/master/src/LibDeclarations/webxr.d.ts
+declare type XREventType =
+    | "devicechange"
+    | "visibilitychange"
+    | "end"
+    | "inputsourceschange"
+    | "select"
+    | "selectstart"
+    | "selectend"
+    | "squeeze"
+    | "squeezestart"
+    | "squeezeend"
+    | "reset"
+
+declare interface XRSessionEvent extends Event {
+    readonly session: XRSession
+}
+
+declare type XRHitTestMode = "realworld" | "virtual"
+
+declare type SessionVRInputType = "controller" | "hand"
+
+// VR
+
+// VR SESSION - Configuration
+
+declare type XRHandProfile = "boxes" | "spheres" | "oculus"
+declare type XRHandTouchEnabled = "left" | "right" | "both"
+
+declare interface XRHandTouchConfigHandsItem {
+    hand: XRHandTouchEnabled
+    index: number[]
+}
+
+declare interface XRHandTouchConfigHands extends Array<XRHandTouchConfigHandsItem> {}
+
+/*
+--------- VR PINCH ---------
+*/
+
+// VR PINCH - Configuration
+
+declare type XRHandPinchMode = "remote" | "touch" | "hybrid"
+
+declare interface XRHandPinchConfigItem {
+    hand: XRHandTouchEnabled
+    mode: XRHandPinchMode
+    distance?: {
+        remote?: number
+        touch?: number
+    }
+    materials?: {
+        remote?: XRHandPinchRayMaterial
+        touch?: XRHandPinchRayMaterial
+    }
+    colors?: {
+        remote?: number
+        remoteHit?: number
+        touch?: number
+        touchHit?: number
+    }
+}
+
+declare interface XRHandPinchConfig extends Array<XRHandPinchConfigItem> {}
+
+// VR PINCH - Debugger
+
+declare type XRHandPinchRayMaterial = LineDashedMaterial | LineBasicMaterial
+
+/*
+--------- VR TOUCH (HANDS) ----------
+*/
+
+// VR HAND TOUCH - Configuration
+
+// Aavailable Hit-Test Modes
+
+declare type XRHandTouchTestMode = "ray" | "sphere"
+
+// Configuration Prop passed to a 'SessionVR' component
+
+declare interface XRHandTouchConfig {
+    mode?: XRHandTouchTestMode
+    sphereRadius?: number
+    touchDistance?: number
+    lerpFactor?: number
+    debug?: XRHandTouchDebugParams
+    hands?: XRHandTouchConfigHands
+}
+
+// Update Props passed to an 'XRHandTouchRayExt' or 'XRHandTouchSphereExt' instance on every XRFrame --> @see 'SessionVR'
+
+declare interface XRTouchRayUpdateParams {
+    handProfile: XRHandProfile
+    raycaster: Raycaster
+    xrFrameDelta: number
+}
+
+declare interface XRTouchSphereUpdateParams {
+    handProfile: XRHandProfile
+    raycaster: Raycaster
+    xrFrameDelta: number
+}
+
+// VR HAND TOUCH - Events
+
+declare interface XRHandTouchEventsItem {
+    name: string
+    hand: XRHandTouchEnabled
+    distance?: number
+    touchtime?: number
+    index: number[]
+}
+
+declare interface XRHandTouchEvents extends Array<XRHandTouchEventsItem> {}
+
+// VR HAND TOUCH - Debugger Configuration
+
+declare interface XRHandTouchRayDebuggerConfig {
+    drawTentacles: boolean
+    // TODO: Rename this
+    drawTouchDebuggers: boolean
+}
+
+declare interface XRHandTouchSphereDebuggerConfig {
+    widthSegments: number
+    heightSegments: number
+    colors: {
+        touch: number
+    }
+    mat: { [key: string]: any }
+}
+
+declare interface XRHandTouchDebugConfigItem {
+    mode: XRHandTouchTestMode
+    config?: XRHandTouchRayDebuggerConfig | XRHandTouchSphereDebuggerConfig
+}
+
+declare interface XRHandTouchDebugConfig extends Array<XRHandTouchDebugConfigItem> {}
+
+declare interface XRHandTouchDebugParams {
+    enabled: boolean
+    debugConfig: XRHandTouchDebugConfig
+    hightlightJoints?: {
+        enabled: boolean
+        colors?: {
+            normal?: number
+            touch?: number
+        }
+    }
+    colorFaces?: {
+        enabled: boolean
+        colors?: {
+            touch?: number
+            unTouch?: number
+            touchThroughEnter?: number
+            touchThroughExit?: number
+            scratch?: number
+        }
+    }
+}
+
+// VR HAND TOUCH X
+
+// VR HAND TOUCH X - Configuration
+
+declare interface XRHandTouchXIndexItem {
+    left: number[]
+    right: number[] | number[]
+}
+
+declare interface XRHandTouchXIndexPairs extends Array<XRHandTouchXIndexItem> {}
+
+declare interface XRHandTouchXConfigItem {
+    hand: XRHandTouchEnabled
+    name: string
+    distance?: number
+    touchtime?: number
+    indexPairs: XRHandTouchXIndexPairs
+}
+
+declare interface XRHandTouchXConfig extends Array<XRHandTouchXConfigItem> {}
