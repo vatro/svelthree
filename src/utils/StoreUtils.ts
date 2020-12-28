d5bd2e086ac94f760d04a1de8335e07439b54a16
Vatroslav Vrbanic <vatro@users.noreply.github.com>
2020-12-28 02:52:39 +0100

    Create StoreUtils.ts

---
+++ src/utils/StoreUtils.ts
@@ -0,0 +1,37 @@
+import { Scene } from "svelthree-three"
+
+export default class StoreUtils {
+    public static getSTIfromScene(scene: Scene, component: string): number {
+        if (scene) {
+            if (scene.type === "Scene") {
+                return this.getSTI(scene, component)
+            } else {
+                console.warn(`SVELTHREE > ${component} : You have to provide a valid 'scene' prop of type 'Scene'!`, {
+                    scene: scene
+                })
+                throw new Error("SVELTHREE Exception (see warning above)")
+            }
+        } else {
+            console.warn(`SVELTHREE > ${component} : You have to provide a {scene} prop!`, {
+                scene: scene
+            })
+            throw new Error(`SVELTHREE Exception (see warning above)`)
+        }
+    }
+
+    private static getSTI(scene: Scene, component: string): number {
+        if (scene.userData.sti >= 0) {
+            return scene.userData.sti
+        } else {
+            console.warn(
+                `SVELTHREE > ${component} : Failed to set 'sti' from 'scene.userData.sti', 'sti' has to be >= 0!`,
+                {
+                    scene: scene,
+                    userData: scene.userData,
+                    sti: scene.userData.sti
+                }
+            )
+            throw new Error("SVELTHREE Exception (see warning above)")
+        }
+    }
+}
