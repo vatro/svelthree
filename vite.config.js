83819c96b03b959415a95dbb6b4868d158822740
Vatroslav Vrbanic <v.vrbanic@cream.gmbh>
2021-11-29 19:27:51 +0100

    [project setup] Remove hot: {acceptAccessors: true} from vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,11 +7,7 @@ import sveltePreprocess from "svelte-preprocess"
 export default defineConfig({
 	plugins: [
 		svelte({
-			preprocess: sveltePreprocess(),
-			// see https://github.com/sveltejs/vite-plugin-svelte/blob/93ca647767c683d9f8996e670ae5c1b8171adc21/packages/vite-plugin-svelte/src/utils/options.ts#L260
-			hot: {
-				acceptAccessors: true
-			}
+			preprocess: sveltePreprocess()
 		})
 	],
 	build: {
