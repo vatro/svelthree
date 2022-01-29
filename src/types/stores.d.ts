103a2899722679b37117114c87f0862f7aef738a
Vatroslav Vrbanic <v.vrbanic@cream.gmbh>
2022-01-29 02:36:50 +0100

    [ts] Remove obsolete types folder

--- src/types/stores.d.ts
+++
@@ -1,3 +0,0 @@
-import type { Writable } from "svelte/store"
-
-export declare const svelthreeStores: Writable<any[]>
--- src/types/three-mesh-bvh/index.d.ts
+++
@@ -1,5 +0,0 @@
-declare module "three-mesh-bvh" {
-	export function computeBoundsTree(options: { [key: string]: any }): any // MeshBVH
-	export function disposeBoundsTree(): void
-	export function acceleratedRaycast(raycaster: THREE.Raycaster, intersects: THREE.Intersection[]): void
-}
