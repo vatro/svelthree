4e680dab1645279e0f7c198fa218697672bf2992
Vatroslav Vrbanic <v.vrbanic@cream.gmbh>
2022-02-22 14:38:24 +0100

    Refactor 'RaycastArray.ts'
    Refactor RaycastArray:
    
    Functionality now more similar to 'SvelthreeStoreArray'
    - on 'push(...)': index of the element ('index_in_raycast') is being saved inside object instance's 'userData' for better splicing behavior.
    - if items are being pushed or spliced, the array will be marked as 'dirty', which will be used for allowing (conditional) re-filtering of the 'filtered_raycast' array in the Canvas component.

--- src/utils/RaycastArray.ts
+++ src/utils/RaycastArray.ts
@@ -2,6 +2,9 @@ import type { Object3D } from "three"
 import type { RaycastableSvelthreeComponents } from "../types-extra"
 
 class RaycastArray_Base extends Array {
+   public head_path?: string[] | undefined = undefined
+   public index_prop: string | undefined = undefined
+   public dirty: boolean = false
 
    constructor(...args: any[]) {
       super(...args)
@@ -10,7 +13,16 @@ class RaycastArray_Base extends Array {
    push(...to_check_intersection: (Object3D | RaycastableSvelthreeComponents)[]): number {
       super.push(...to_check_intersection)
       this.process_to_check_intersection()
+      this.set_index_prop()
+      this.dirty = true
       return this.length
+   }
+
+   splice(start: number, deleteCount?: number): any[] {
+      super.splice(start, deleteCount)
+      this.set_index_prop()
+      this.dirty = true
+      return this
    }
 
    // replace component references with three.js object instances created by the component 
@@ -21,14 +33,32 @@ class RaycastArray_Base extends Array {
          }
       }
    }
+
+   set_index_prop() {
+      for (let i = 0; i < this.length; i++) {
+         let prop_head: any
+
+         if (this.head_path?.length) {
+            prop_head = this[i][this.head_path[0]]
+
+            let j = 1
+            while (j < this.head_path.length) {
+               prop_head = prop_head[this.head_path[j]]
+               j++
+            }
+         }
+
+         if (prop_head && this.index_prop) {
+            prop_head[this.index_prop] = i
+         }
+      }
+   }
 }
 
 
 class RaycastArray extends RaycastArray_Base {
-   public to_check_intersection: (Object3D | RaycastableSvelthreeComponents)[]
-   constructor(...to_check_intersection: (Object3D | RaycastableSvelthreeComponents)[]) {
+   constructor(public index_prop: string = "index_in_raycast", public head_path: string[] = ["userData"]) {
       super()
-      this.to_check_intersection = to_check_intersection
    }
 }
 
