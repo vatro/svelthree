d19440e2e9f345940857e1d19b0c144ec976af00
Vatroslav Vrbanic <v.vrbanic@cream.gmbh>
2022-02-22 15:20:14 +0100

    [Canvas] Introduce public function 'get_interactive_all()'

--- src/components/Canvas.svelte
+++ src/components/Canvas.svelte
@@ -407,6 +407,11 @@ This is a **svelthree** _Canvas_ Component.
 		})
 	}
 
+	/** Get all interactive **three.js object instances** from all Scenes inside the Canvas. */
+	export function get_interactive_all(): Object3D[] {
+		return [...raycast]
+	}
+
 	function set_cursor_style(css_value: string): void {
 		// doesn't update the component on cursor change (as opposed to c.style)
 		self.$$.root.style.cursor = css_value
