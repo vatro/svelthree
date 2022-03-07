7091151995edf1363c384f4c4ff8dab12cfc4787
Vatroslav Vrbanic <v.vrbanic@cream.gmbh>
2022-03-07 17:35:37 +0100

    [SvelthreeInteraction] Add 'raycasterData' interface

--- src/components-internal/SvelthreeInteraction.svelte
+++ src/components-internal/SvelthreeInteraction.svelte
@@ -244,7 +244,19 @@ This is a **svelthree** _SvelthreeInteraction_ Component.
 	let isOverDispatched = false
 	let isOutDispatched = true
 
-	let raycasterData: {}
+	
+	interface RaycasterData {
+		/** `intersections` are of the same form as those returned by [`.intersectObject`](https://threejs.org/docs/#api/en/core/Raycaster.intersectObject). */
+		intersections: { [P in keyof THREE.Intersection]: THREE.Intersection[P] },
+		/** Current `Raycaster` `.ray`, e.g. useful properties: `ray.origin: Vector3` | `ray.direction: Vector3`. */
+		ray: THREE.Ray,
+		/** The `Camera` used for raycasting. */
+		camera: THREE.Camera,
+		/** Current pointer position ( _'point' / Vector3 position_ ) in 3d world space. */
+		unprojectedPoint: THREE.Vector3
+	}
+
+	let raycasterData: RaycasterData
 
 	function check_overout(e: PointerEvent) {
 		if (intersects()) {
