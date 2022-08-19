80139e7b3e4736dd640b57cc2c455f5ba6eae968
Vatroslav Vrbanic <v.vrbanic@cream.gmbh>
2022-08-20 01:52:15 +0200

    `OrbitControls` remove unused `browser` import (#143)

--- src/components/OrbitControls.svelte
+++ src/components/OrbitControls.svelte
@@ -31,16 +31,6 @@ svelthree uses svelte-accmod, where accessors are always `true`, regardless of `
 	import type { default as OrthoCamSvelthreeComponent } from "./OrthographicCamera.svelte"
 	import type { default as CanvasSvelthreeComponent } from "../components/Canvas.svelte"
 
-	/**
-	 *  SVELTEKIT  SSR /
-	 * `browser` is needed for the SvelteKit setup (SSR / CSR / SPA).
-	 * For non-SSR output in RollUp only and Vite only setups (CSR / SPA) we're just mimicing `$app/env` where `browser = true`,
-	 * -> TS fix: `$app/env` mapped to `src/$app/env` via svelthree's `tsconfig.json`'s `path` property.
-	 * -> RollUp only setup: replace `$app/env` with `../$app/env`
-	 * The import below will work out-of-the-box in a SvelteKit setup.
-	 */
-	import { browser } from "$app/env"
-
 	const self = get_current_component()
 	const c_name = get_comp_name(self)
 
