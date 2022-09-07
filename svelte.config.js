b7d677931e5981a90025079e0f0da82982f1acbc
Vatroslav Vrbanic <v.vrbanic@cream.gmbh>
2022-09-07 22:56:15 +0200

    Add `svelte.config.js` (#161)

---
+++ svelte.config.js
@@ -0,0 +1,37 @@
+import adapter from "@sveltejs/adapter-auto"
+import preprocess from "svelte-preprocess"
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: preprocess(),
+
+	kit: {
+		adapter: adapter(),
+
+		alias: {
+			"$app/env": "./src/lib/$app/env/index.ts",
+			"svelthree/stores": "./src/lib/stores/index.ts",
+			svelthree: "./src/lib/index.ts",
+			"svelthree/*": "./src/lib/*"
+		}
+	},
+
+	package: {
+		dir: "package",
+		emitTypes: true,
+		// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+		exports: (filepath) => {
+			console.log(filepath)
+			if (filepath.startsWith(`index.ts`)) return true
+			if (filepath.startsWith(`utils/index`)) return true
+			//if (filepath.startsWith(`logic/if$/index`)) return true
+			//if (filepath.startsWith(`logic/shared/index`)) return true
+			//if (filepath.startsWith(`logic/special/index`)) return true
+			if (filepath.startsWith(`stores/index`)) return true
+		}
+	}
+}
+
+export default config
