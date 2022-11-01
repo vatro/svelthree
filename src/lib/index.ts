677ec22f6e0a2149db89c26d8d2767c6d1c4f1dd
Vatroslav Vrbanic <v.vrbanic@cream.gmbh>
2022-11-02 00:28:45 +0100

    `index` Don't `export * from "three"` or `THREE.Foo` as `_Foo` (closes #182)

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -18,31 +18,6 @@ export { default as WebGLRenderer } from "./components/WebGLRenderer.svelte"
 export { default as OrbitControls } from "./components/OrbitControls.svelte"
 export { default as CubeCamera } from "./components/CubeCamera.svelte"
 
-// export all THREE modules
-export * from "three"
-
-// TODO  export as `THREE_*`?
-/** export all THREE modules 'Foo' overridden by Svelte components as '_Foo'
- * so users can for example import _Mesh from 'svelthree' and create a native THREE Mesh instance
- */
-export { Mesh as _Mesh } from "three"
-export { Points as _Points } from "three"
-export { Object3D as _Object3D } from "three"
-export { Group as _Group } from "three"
-export { AmbientLight as _AmbientLight } from "three"
-export { HemisphereLight as _HemisphereLight } from "three"
-export { DirectionalLight as _DirectionalLight } from "three"
-export { PointLight as _PointLight } from "three"
-export { RectAreaLight as _RectAreaLight } from "three"
-export { SpotLight as _SpotLight } from "three"
-export { Camera as _Camera } from "three"
-export { PerspectiveCamera as _PerspectiveCamera } from "three"
-export { OrthographicCamera as _OrthographicCamera } from "three"
-export { CubeCamera as _CubeCamera } from "three"
-export { Scene as _Scene } from "three"
-export { OrbitControls as _OrbitControls } from "three/examples/jsm/controls/OrbitControls"
-export { WebGLRenderer as _WebGLRenderer } from "three"
-
 // generic type utilities
 export type { prop } from "./types/types-extra"
 export type { PropMat } from "./types/types-comp-props"
