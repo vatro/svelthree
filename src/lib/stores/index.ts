780d4792d4cff1bd4a63e839af2e77b726760f5f
Vatroslav Vrbanic <v.vrbanic@cream.gmbh>
2022-11-17 15:54:58 +0100

    Change `svelthreeStores`'s type to include `null` values (#153)

--- src/lib/stores/index.ts
+++ src/lib/stores/index.ts
@@ -2,5 +2,4 @@ import { writable } from "svelte/store"
 import type { Writable } from "svelte/store"
 import type { StoreBody } from "../types/types-extra"
 
-//export const svelthreeStores: Writable<(StoreBody | null)[]> = writable([])
-export const svelthreeStores: Writable<StoreBody[]> = writable([])
+export const svelthreeStores: Writable<(StoreBody | null)[]> = writable([])
